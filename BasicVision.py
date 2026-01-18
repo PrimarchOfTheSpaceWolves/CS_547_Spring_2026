5efa0f1ff673bff5d719ee3320aa36bc3bc44d22
Dr. Michael Reale <realemj@sunypoly.edu>
2026-01-18 18:17:41 -0500

    Initial commit

---
+++ BasicVision.py
@@ -0,0 +1,107 @@
+###############################################################################
+# IMPORTS
+###############################################################################
+
+import sys
+import numpy as np
+import torch
+import cv2
+import pandas
+import sklearn
+import timm
+import torchvision
+
+###############################################################################
+# MAIN
+###############################################################################
+
+def main():        
+    ###############################################################################
+    # PYTORCH
+    ###############################################################################
+    
+    b = torch.rand(5,3)
+    print("Random Torch Numbers:")
+    print(b)
+    print("Do you have Torch CUDA/ROCm?:", torch.cuda.is_available())
+    print("Do you have Torch MPS?:", torch.mps.is_available())
+    
+    ###############################################################################
+    # PRINT OUT VERSIONS
+    ###############################################################################
+
+    print("Torch:", torch.__version__)
+    print("TorchVision:", torchvision.__version__)
+    print("timm:", timm.__version__)
+    print("Numpy:", np.__version__)
+    print("OpenCV:", cv2.__version__)
+    print("Pandas:", pandas.__version__)
+    print("Scikit-Learn:", sklearn.__version__)
+        
+    ###############################################################################
+    # OPENCV
+    ###############################################################################
+    if len(sys.argv) <= 1:
+        # Webcam
+        print("Opening the webcam...")
+
+        # Linux/Mac (or native Windows) with direct webcam connection
+        camera = cv2.VideoCapture(0, cv2.CAP_DSHOW) # CAP_DSHOW recommended on Windows 
+                
+        # Did we get it?
+        if not camera.isOpened():
+            print("ERROR: Cannot open the camera!")
+            exit(1)
+
+        # Create window ahead of time
+        windowName = "Webcam"
+        cv2.namedWindow(windowName)
+
+        # While not closed...
+        key = -1
+        while key == -1:
+            # Get next frame from camera
+            _, image = camera.read()
+            
+            # Show the image
+            cv2.imshow(windowName, image)
+
+            # Wait 30 milliseconds, and grab any key presses
+            key = cv2.waitKey(30)
+
+        # Release the camera and destroy the window
+        camera.release()
+        cv2.destroyAllWindows()
+
+        # Close down...
+        print("Closing application...")
+
+    else:
+        # Trying to load image from argument
+
+        # Get filename
+        filename = sys.argv[1]
+
+        # Load image
+        print("Loading image:", filename)
+        image = cv2.imread(filename) 
+        
+        # Check if data is invalid
+        if image is None:
+            print("ERROR: Could not open or find the image!")
+            exit(1)
+
+        # Show our image (with the filename as the window title)
+        windowTitle = "PYTHON: " + filename
+        cv2.imshow(windowTitle, image)
+
+        # Wait for a keystroke to close the window
+        cv2.waitKey(-1)
+
+        # Cleanup this window
+        cv2.destroyAllWindows()
+
+# The main function
+if __name__ == "__main__": 
+    main()
+    
---
+++ README.md
@@ -0,0 +1,12 @@
+# CS 547: Computer Vision and Image Processing
+***Spring 2026***  
+***Author: Your Name Here***  
+***Original Author: Dr. Michael J. Reale***  
+***SUNY Polytechnic Institute*** 
+
+## Runnable Python Scripts
+
+### BasicVision.py
+A basic sample that loads up the relevant libraries, prints versions numbers, and either 1) loads an image from a path specified on the command line, or 2) opens a webcam.
+Image(s) will be displayed until a key is hit.
+
