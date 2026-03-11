c42390f46616059077d518be6e76fcb9cd197c90
Dr. Michael Reale <realemj@sunypoly.edu>
2026-03-11 17:14:27 -0400

    Fourier exercises

--- ProfExercises_Frequency.py
+++ ProfExercises_Frequency.py
@@ -11,6 +11,31 @@ import sklearn
 import timm
 import torchvision
 
+left_mouse_down = False
+right_mouse_down = False
+
+def on_mouse(event, x, y, flags, param):
+    global left_mouse_down
+    global right_mouse_down
+    
+    def draw_to_mask_image(param, x, y, fill_value=0.0):
+        mask_image = param[0]
+        circle_radius = param[1]
+        cv2.circle(mask_image, (x,y), circle_radius, fill_value, -1)
+    
+    if event == cv2.EVENT_LBUTTONUP:
+        left_mouse_down = False
+    elif event == cv2.EVENT_RBUTTONUP:
+        right_mouse_down = False
+    elif (event == cv2.EVENT_LBUTTONDOWN or 
+          (left_mouse_down and event == cv2.EVENT_MOUSEMOVE)):
+        left_mouse_down = True
+        draw_to_mask_image(param, x, y, fill_value=0.0)
+    elif (event == cv2.EVENT_RBUTTONDOWN or 
+          (right_mouse_down and event == cv2.EVENT_MOUSEMOVE)):
+        right_mouse_down = True
+        draw_to_mask_image(param, x, y, fill_value=1.0)   
+
 def to_numpy_complex(complex_image):
     return complex_image[:,:,0] + 1j*complex_image[:,:,1]
 
@@ -32,10 +57,105 @@ def make_simple_complex(length=600):
     complex_data += np.reshape(1j*values, (-1, 1))    
     return complex_data
 
-def do_frequency(image, mask_image):
-    output = np.copy(image)
-    # TODO
+def compute_fourier(image, nonzeroRows=0):
+    fimage = image.astype("float64")
+    return cv2.dft(fimage, flags=cv2.DFT_COMPLEX_OUTPUT, nonzeroRows=nonzeroRows)
+
+def compute_inverse_fourier(complex_image, nonzeroRows=0):
+    return cv2.idft(complex_image, 
+                    flags=cv2.DFT_REAL_OUTPUT+cv2.DFT_SCALE,
+                    nonzeroRows=nonzeroRows)
+    
+def make_display_magnitude(mag):
+    display_mag = cv2.log(mag + 1.0)
+    cv2.normalize(display_mag, display_mag, norm_type=cv2.NORM_MINMAX)
+    return display_mag
+
+def image_space_shift(image):
+    rows, cols = image.shape[:2]
+    row_powers = (-1)**np.arange(rows)[:,None]
+    col_powers = (-1)**np.arange(cols)[None,:]
+    neg_powers = row_powers * col_powers
+    output = image * neg_powers
     return output
+
+def polar_to_complex(mag, phase):
+    return mag * np.exp(1j*phase)
+
+def filter_with_fourier(image, kernel, mask_image=None):
+    padded_rows = cv2.getOptimalDFTSize(image.shape[0]+kernel.shape[0]-1)
+    padded_cols = cv2.getOptimalDFTSize(image.shape[1]+kernel.shape[1]-1)
+    
+    fimage = image.astype("float64")
+    fkernel = kernel.astype("float64")
+    
+    padded_image = cv2.copyMakeBorder(fimage,
+                                      0, padded_rows - fimage.shape[0],
+                                      0, padded_cols - fimage.shape[1],
+                                      borderType=cv2.BORDER_CONSTANT,
+                                      value=0)
+    padded_kernel = cv2.copyMakeBorder(fkernel,
+                                      0, padded_rows - fkernel.shape[0],
+                                      0, padded_cols - fkernel.shape[1],
+                                      borderType=cv2.BORDER_CONSTANT,
+                                      value=0)
+    
+    padded_image = image_space_shift(padded_image)
+    padded_kernel = image_space_shift(padded_kernel)
+    
+    F = compute_fourier(padded_image)
+    H = compute_fourier(padded_kernel)
+    
+    if mask_image is None:
+        mask_image = np.ones(H.shape[:2], dtype="float64")
+    
+    cv2.namedWindow("H magnitude")
+    mask_pack = [mask_image, 50]
+    cv2.setMouseCallback("H magnitude", on_mouse, mask_pack)
+    
+    H_data = to_numpy_complex(H)
+    H_mag, H_phase = complex_to_polar(H_data)
+    #print(H_mag.shape, mask_image.shape)
+    H_mag = H_mag * mask_image
+    H_data = polar_to_complex(H_mag, H_phase)
+    H = to_complex_image(H_data)
+        
+    G = cv2.mulSpectrums(F,H,0,conjB=False)
+    
+    def really_make_display_magnitude(fourier):
+        return make_display_magnitude(complex_to_polar(to_numpy_complex(fourier))[0])
+    
+    display_F_mag = really_make_display_magnitude(F)
+    display_H_mag = really_make_display_magnitude(H)
+    display_G_mag = really_make_display_magnitude(G)
+    
+    cv2.imshow("F magnitude", display_F_mag)
+    cv2.imshow("H magnitude", display_H_mag)
+    cv2.imshow("G magnitude", display_G_mag)
+    
+    padded_output = compute_inverse_fourier(G, nonzeroRows=padded_image.shape[0])
+    padded_output = image_space_shift(padded_output)
+    
+    sr = int(kernel.shape[0]/2)
+    er = sr + image.shape[0]
+    sc = int(kernel.shape[1]/2)
+    ec = sc + image.shape[1]
+    
+    output = padded_output[sr:er, sc:ec]
+    output /= 255.0
+    
+    return output, mask_image
+
+def make_gaussian_filter(sidelen):
+    gaussianCol = cv2.getGaussianKernel(ksize=sidelen, sigma=0)
+    gaussianRow = np.transpose(gaussianCol)
+    kernel = np.matmul(gaussianCol, gaussianRow)
+    return kernel
+
+def do_frequency(image, mask_image):
+    kernel = make_gaussian_filter(3)
+    output, mask_image = filter_with_fourier(image, kernel, mask_image)
+    return output, mask_image
 
 ###############################################################################
 # MAIN
@@ -43,6 +163,7 @@ def do_frequency(image, mask_image):
 
 def main():  
     
+    '''
     complex_data = make_simple_complex(600)
     mag, phase = complex_to_polar(complex_data)
     complex_image = to_complex_image(complex_data)
@@ -59,6 +180,7 @@ def main():
     cv2.destroyAllWindows()    
     
     exit()
+    '''
     
     mask_image = None
           
@@ -111,10 +233,10 @@ def main():
             _, image = camera.read()
             grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             
-            if mask_image is None:
-                mask_image = np.ones(grayscale.shape, dtype="float64")
+            #if mask_image is None:
+            #    mask_image = np.ones(grayscale.shape, dtype="float64")
             
-            output = do_frequency(grayscale, mask_image)
+            output, mask_image = do_frequency(grayscale, mask_image)
             
             # Show the image
             cv2.imshow(windowName, image)
